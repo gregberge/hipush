d7509b10e663f26d9402037210a63c423376e27e
Greg Bergé <neoziro@gmail.com>
2015-01-22 18:28:48 +0100

    Add task to populate database.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,26 @@ var mocha = require('gulp-mocha');
 
 gulp.task('db:sync', function () {
   var models = require('./lib/models');
-  return models.sequelize.sync()
-  .then(function () {
-    return models.sequelize.close();
+  return models.sequelize.sync();
+});
+
+gulp.task('db:populate', ['db:sync'], function () {
+  var Promise = require('bluebird');
+  var models = require('./lib/models');
+
+  return Promise.all([
+    models.Customer.create({
+      id: 1,
+      email: 'berge.greg@gmail.com'
+    }),
+    models.Website.create({
+      id: 1,
+      name: 'Hipush',
+      domain: 'http://hipush.net'
+    })
+  ])
+  .spread(function (customer, website) {
+    return website.setCustomer(customer);
   });
 });
 
