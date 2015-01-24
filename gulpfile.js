2d0738655766738de3cd07ff33a3bbd939c0ee7b
Greg Bergé <neoziro@gmail.com>
2015-01-24 13:08:41 +0100

    Remove gulp-mocha, use mocha to run tests.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
 
 gulp.task('db:sync', function () {
   var models = require('./lib/models');
@@ -23,19 +22,5 @@ gulp.task('db:populate', ['db:sync'], function () {
   ])
   .spread(function (customer, website) {
     return website.setCustomer(customer);
-  });
-});
-
-gulp.task('test', function () {
-  // Force test environment.
-  process.env.NODE_ENV = 'test';
-
-  return gulp.src('test/lib/**/*.js', {read: false})
-  .pipe(mocha())
-  .once('error', function () {
-    process.exit(1);
-  })
-  .once('end', function () {
-    process.exit();
   });
 });
