91782f485da12a4cad8cbabafd79875f0c3ba201
Greg Bergé <neoziro@gmail.com>
2015-01-30 18:19:09 +0100

    Fix config spn.

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   spn: {
     packageDirectory: '/var/hipush/packages',
     imageDirectory: '/var/hipush/images',
-    cert: '/etc/hipush/ssl/hipush.crt',
-    key: '/etc/hipush/ssl/hipush.key'
+    cert: '/etc/hipush/spn/hipush.crt',
+    key: '/etc/hipush/spn/hipush.key'
   }
 };
