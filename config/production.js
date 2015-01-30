86ebf54ed3b432d6148e53018a0d4ff51c289714
Greg Bergé <neoziro@gmail.com>
2015-01-30 16:35:22 +0100

    Specify production config.

--- config/production.js
+++ config/production.js
@@ -1,2 +1,8 @@
 module.exports = {
+  spn: {
+    packageDirectory: '/var/hipush/packages',
+    imageDirectory: '/var/hipush/images',
+    cert: '/etc/hipush/ssl/hipush.crt',
+    key: '/etc/hipush/ssl/hipush.key'
+  }
 };
