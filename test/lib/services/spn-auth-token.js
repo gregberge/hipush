6521505dee3578fcd81aba9d46850352e2c12f3c
Greg Bergé <neoziro@gmail.com>
2015-01-30 15:08:10 +0100

    Update spn auth token.

--- test/lib/services/spn-auth-token.js
+++ test/lib/services/spn-auth-token.js
@@ -2,9 +2,9 @@ var expect = require('chai').expect;
 var spnAuthToken = require('../../../lib/services/spn-auth-token');
 
 describe('Spn auth token', function () {
-  it('should crypt and decrypt', function () {
-    var crypted = spnAuthToken.crypt(20);
-    var decrypted = spnAuthToken.decrypt(crypted);
-    expect(decrypted).to.equal(20);
+  it('should encode and decode', function () {
+    var token = spnAuthToken.encode({websiteId: 20});
+    var payload = spnAuthToken.decode(token);
+    expect(payload).to.eql({websiteId: 20});
   });
 });
