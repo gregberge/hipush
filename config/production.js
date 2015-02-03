e54dde34532083572d1db84dc888001528856db0
Greg Bergé <neoziro@gmail.com>
2015-02-03 16:38:21 +0100

    Log to logstash.

--- config/production.js
+++ config/production.js
@@ -4,5 +4,8 @@ module.exports = {
     imageDirectory: '/var/hipush/images',
     cert: '/etc/hipush/spn/hipush.crt',
     key: '/etc/hipush/spn/hipush.key'
+  },
+  logger: {
+    logstash: true
   }
 };
